69157a06a6a5e429eeb1d0781974ff2eb36a6bec
Kingston <chongkingston391@gmail.com>
2026-05-15 21:21:13 +0800

    fix

--- app/summary/layout.jsx
+++ app/summary/layout.jsx
@@ -1,10 +1,18 @@
 import AppShell from "../components/AppShell";
 
+const FONTS_URL =
+  "https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600&family=Fraunces:ital,opsz,wght@0,9..144,300;0,9..144,600;1,9..144,300&display=swap";
+
+/** Summary routes: load dashboard fonts early (same URL as dashboard page.jsx). */
 export default function SummaryLayout({ children }) {
   return (
-    <AppShell showBackToDashboard showSidebar hidePrevUploads>
-      {children}
-    </AppShell>
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href={FONTS_URL} rel="stylesheet" />
+      <AppShell showBackToDashboard showSidebar hidePrevUploads>
+        {children}
+      </AppShell>
+    </>
   );
 }
-
