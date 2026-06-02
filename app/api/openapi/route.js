c525d6173dab1ce899172be7615bc66df972bce2
Kingston <chongkingston391@gmail.com>
2026-06-02 13:34:35 +0800

    update dir

--- app/api/openapi/route.js
+++
@@ -1,7 +0,0 @@
-import { NextResponse } from "next/server";
-import { openApiSpec } from "@/lib/openapi";
-import { apiHandler } from "@/lib/apiHandler";
-
-export const GET = apiHandler(async function GET() {
-  return NextResponse.json(openApiSpec);
-});
